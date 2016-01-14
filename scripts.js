5c0e893291d29227c5b307aa1105ed09197bd65d
franksal82 <franksalazar82@gamil.com>
2016-01-13 20:36:14 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//ask for your first name
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+console.log(firstName);
+var done = confirm("Studies show that clicking OK is good for your health");
