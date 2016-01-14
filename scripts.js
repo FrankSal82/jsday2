3738988b5021565f6d4194fbfe2039ebe0683fa3
franksal82 <franksalazar82@gamil.com>
2016-01-13 20:49:52 -0800

    added comments

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //ask for your first name
 var firstName = prompt("What is your first name?");
+//ask for your last name
 var lastName = prompt("What is your last name?");
+//print first name in console
 console.log(firstName);
+//confirm something
 var done = confirm("Studies show that clicking OK is good for your health");
